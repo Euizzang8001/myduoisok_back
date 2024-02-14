5c06f581f5e26abef4cf3e415bee584389eb73e2
Euizzang8001 <140701211+Euizzang8001@users.noreply.github.com>
2024-02-14 17:24:11 +0900

    Update databases.py

--- app/config/databases.py
+++ app/config/databases.py
@@ -1,10 +1,5 @@
-import json
+from pymongo.mongo_client import MongoClient
 import os
-from pymongo import MongoClient
-from dotenv import load_dotenv
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 DB_USER=os.environ["DB_USER"]
 DB_PASSWORD=os.environ["DB_PASSWORD"]
@@ -12,12 +7,16 @@ DB_HOST=os.environ["DB_HOST"]
 DB_PORT=os.environ["DB_PORT"]
 DB_DATABASE=os.environ["DB_DATABASE"]
 
-client = MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@myduoisok.gercpam.mongodb.net/?retryWrites=true&w=majority") 
+uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_DATABASE}.gercpam.mongodb.net/?retryWrites=true&w=majority"
 
-db = client.myduoisok
+# Create a new client and connect to the server
+client = MongoClient(uri)
 
-
-
-
-
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
     
+db = client.myduoisok
